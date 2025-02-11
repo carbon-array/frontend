3c51d0b3643afd516d8edf5eade913fe5bc2cb8a
Daivik Dhar <vikdai217@gmail.com>
2025-02-11 09:04:59 -0500

    ading boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@shadcn/ui';
+
+function App() {
+  return (
+    <div className="App">
+      <Button className="bg-blue-500 text-white p-2 rounded">ShadCN Button</Button>
+    </div>
+  );
+}
+
+export default App;

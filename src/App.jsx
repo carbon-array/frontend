5059feec7982643bf150f34f0861f305ac81e963
Daivik Dhar <vikdai217@gmail.com>
2025-02-17 21:51:18 -0500

    added signup and login routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/footer.jsx";
 import Hero from "./components/hero.jsx";
 import Connectors from "./components/connectors.jsx";
-import Text from "./components/text.jsx"; 
+import Text from "./components/text.jsx";
 import FAQ from "./components/faqs.jsx";
+import Login from "./components/login.jsx"; // Import the Login page
+import Signup from "./components/signup.jsx"; // Import the Signup page
 
 function App() {
   const [scrollY, setScrollY] = useState(0);
@@ -18,23 +21,36 @@ function App() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-white text-black">
-      <Navbar />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Hero /> {/* Render the Hero component here */}
-      <br />
-      <br />
-      <br />
-      <br />
-      <Text/>
-
-      <Connectors/>
-      <FAQ />
-      <Footer />
-    </div>
+    <Router>
+      <div className="min-h-screen bg-white text-black">
+        <Navbar />
+        <Routes>
+          {/* Home Page */}
+          <Route
+            path="/"
+            element={
+              <>
+                <br />
+                <br />
+                <br />
+                <br />
+                <Hero />
+                <br />
+                <br />
+                <Text />
+                <Connectors />
+                <FAQ />
+              </>
+            }
+          />
+          {/* Login Page */}
+          <Route path="/login" element={<Login />} />
+          {/* Signup Page */}
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
